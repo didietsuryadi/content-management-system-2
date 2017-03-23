1aee4158c3fc4c812531520c6b531fc657e2b2de
didietsuryadi <didietsuryadi>
2017-03-23 13:17:39 +0700

    create-models-user-and-article

---
+++ server/models/article.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var articleSchema = new Schema({
+  title: String,
+  content: String,
+  author:{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+},{
+  timestamps:true
+})
+
+var Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
