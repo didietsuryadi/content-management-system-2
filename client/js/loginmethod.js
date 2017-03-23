d1c679caa663d95912c81cbc5e8d4d7870a5889e
didietsuryadi <didietsuryadi>
2017-03-23 14:00:40 +0700

    create-login-ui

---
+++ client/js/loginmethod.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    name:'',
+    username: '',
+    password: '',
+    email: ''
+  },
+  methods: {
+    login: function (e) {
+      e.preventDefault()
+      axios.post('http://localhost:3000/login',
+      {
+        username: app.username,
+        password: app.password,
+      }).then(function (res) {
+        if(res.data.token){
+          localStorage.setItem("userid",res.data.userid)
+          localStorage.setItem("username",res.data.username)
+          localStorage.setItem("token", res.data.token)
+          window.location.href = 'http://127.0.0.1:8080/'
+        }else{
+          alert(res.data)
+        }
+        app.resetButton()
+      }),
+    },
+    register: function(e) {
+      e.preventDefault()
+      axios.post('http://localhost:3000/register',
+      {
+        name: app.name,
+        username: app.username,
+        password: app.password,
+        email: app.email
+      }).then(function (res) {
+        if(res.data.username == app.username){
+          alert("account already created, please login")
+        }
+        app.resetButton()
+      })
+    },
+    resetButton: function() {
+      app.name = ''
+      app.username = ''
+      app.password = ''
+      app.email = ''
+    }
+  }
+})
---
+++ client/js/prevent.js
@@ -0,0 +1,3 @@
+if(!localStorage.getItem('token')){
+  window.location.href = 'http://127.0.0.1:8080/login.html'
+}
